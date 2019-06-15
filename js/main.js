372311156e7a0b1113c4b89fad69dec0b24c1f7e
fyeung21 <yeung.fiona@hotmail.com>
2019-06-15 14:39:05 -0700

    loading.gif no longer repeats image on second selection

--- js/main.js
+++ js/main.js
@@ -4,38 +4,38 @@ const $select = $("#selection"),
     $storiesGrid = $(".stories-grid"),
     $blockContainer = $(".block-container");
 
+$loading.append(`<img class="loading-gif" src="../assets/images/ajax-loader.gif">`);
+$loading.hide();
 
 $select.on('change', function () {
     const section = $("#selection option:selected").val();
 
     event.preventDefault();
-    $.ajax ({
+    $.ajax({
         method: "GET",
         dataType: "json",
         url: `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=YUqBlpD8cvotqM6phA6OPbW1Hlm3O07c`,
-        
-        beforeSend: function(){
+
+        beforeSend: function () {
             $loading.show();
-            $loading.append(`<img class="loading-gif" src="../assets/images/ajax-loader.gif">`);
         },
-        complete: function(){
+        complete: function () {
             $loading.hide();
         }
-        
+
     })
-      
-      
-    .done(function(data) { 
-        $storiesGrid.html('');
-        $.each(data.results, function (key, data) {
-            
-            if (key <= 11 && data.multimedia[4].url.length!==0) {
-                $storiesGrid.append(`<a href="${data.url}"><div class="item" style="background-image: url('${data.multimedia[4].url}')"><p class="abstract">${data.abstract}</p></div></a>`);
-            console.log(data.multimedia[4].url);
-            }
+
+        .done(function (data) {
+            $storiesGrid.html('');
+            $.each(data.results, function (key, data) {
+
+                if (key <= 11 && data.multimedia[4].url.length !== 0) {
+                    $storiesGrid.append(`<a href="${data.url}"><div class="item" style="background-image: url('${data.multimedia[4].url}')"><p class="abstract">${data.abstract}</p></div></a>`);
+                    console.log(data.multimedia[4].url);
+                }
+            })
         })
-    })
-    .fail(function () {
-        $blockContainer.append("Sorry there was an error.");
-    });
+        .fail(function () {
+            $blockContainer.append("Sorry there was an error.");
+        });
 });
