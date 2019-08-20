b33cd66f2628df5ea8887f27598cc11b2cca86ac
fyeung21 <yeung.fiona@hotmail.com>
2019-08-19 20:09:03 -0700

    wrap jquery in document ready

--- js/main.js
+++ js/main.js
@@ -1,48 +1,51 @@
-const $select = $("#selection"),
-  $loading = $(".loading"),
-  $storiesGrid = $(".stories-grid"),
-  $logo = $(".logo"),
-  $header = $(".select-container");
+$(document).ready(function () {
 
-$loading.append(
-  `<img class="loading-gif" src="../assets/images/ajax-loader.gif">`
-);
-$loading.hide();
+  const $select = $("#selection"),
+    $loading = $(".loading"),
+    $storiesGrid = $(".stories-grid"),
+    $logo = $(".logo"),
+    $header = $(".select-container");
 
-$select.on("change", function() {
-  const section = $("#selection option:selected").val();
-  $logo.addClass("logo-small");
-  $header.addClass("header-slim");
+  $loading.append(
+    `<img class="loading-gif" src="../assets/images/ajax-loader.gif">`
+  );
+  $loading.hide();
 
-  event.preventDefault();
-  $.ajax({
-    method: "GET",
-    dataType: "json",
-    url: `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=YUqBlpD8cvotqM6phA6OPbW1Hlm3O07c`,
+  $select.on("change", function () {
+    const section = $("#selection option:selected").val();
+    $logo.addClass("logo-small");
+    $header.addClass("header-slim");
 
-    beforeSend: function() {
-      $loading.show();
-    },
-    complete: function() {
-      $loading.hide();
-    }
-  })
+    event.preventDefault();
+    $.ajax({
+      method: "GET",
+      dataType: "json",
+      url: `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=YUqBlpD8cvotqM6phA6OPbW1Hlm3O07c`,
 
-    .done(function(data) {
-      $storiesGrid.html("");
-      $.each(data.results, function(key, data) {
-        if (key <= 11 && data.multimedia.length >= 5) {
-          $storiesGrid.append(
-            `<a target='_blank' rel="noopener noreferrer" href="${data.url}">
+      beforeSend: function () {
+        $loading.show();
+      },
+      complete: function () {
+        $loading.hide();
+      }
+    })
+
+      .done(function (data) {
+        $storiesGrid.html("");
+        $.each(data.results, function (key, data) {
+          if (key <= 11 && data.multimedia.length >= 5) {
+            $storiesGrid.append(
+              `<a target='_blank' rel="noopener noreferrer" href="${data.url}">
             <div class="item" style="background-image: url('${data.multimedia[4].url}')">
             <div class="abstract">
             <p>${data.abstract}</p>
             </div></div></a>`
-          );
-        }
+            );
+          }
+        });
+      })
+      .fail(function () {
+        $storiesGrid.append("Sorry there was an error.");
       });
-    })
-    .fail(function() {
-      $storiesGrid.append("Sorry there was an error.");
-    });
+  });
 });
